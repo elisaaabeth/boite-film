696f64caf3461108e4894f7af21052c26307fb67
lisebichette <elisabethakakpo0590@yahoo.com>
2025-03-01 12:55:57 +0100

    j'ai avancé

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /* ########## slider Home ####### */
 /* ################################## */
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".home ", {
   spaceBetween: 50,
   centeredSlides: true,
   autoplay: {
@@ -12,5 +12,33 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+});
+
+/* ################################## */
+/* ########## A venir  ####### */
+/* ################################## */
+
+var swiper = new Swiper(".container-coming", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 55000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    568: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 5,
+    },
+    968: {
+      slidesPerView: 6,
+    },
   },
 });
